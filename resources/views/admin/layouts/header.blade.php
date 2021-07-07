55cf10660ec444b79e948b8ffffc0de300a1ac96
alfiannuriman <alfian4299@gmail.com>
2021-07-08 06:59:49 +0700

    Initialize

---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,18 @@
+<div class="header bg-primary pb-6">
+  <div class="container-fluid">
+    <div class="header-body">
+      <div class="row align-items-center py-4">
+        <div class="col-lg-12 col-12">
+          <h6 class="h2 text-white d-inline-block mb-0">Dashboard</h6>
+          <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+            <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+              <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+              <li class="breadcrumb-item"><a href="#">Administrator</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
